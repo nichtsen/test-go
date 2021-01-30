6213977a1f7e3516a838e1583664741254abaa1d
nichtsen <hobblong@163.com>
2021-01-30 18:25:01 +0800

    clr

--- customJson/customJson.go
+++ customJson/customJson.go
@@ -5,14 +5,6 @@ import (
 	"time"
 )
 
-type Temple struct {
-	ID int64 `json:"id,string"`
-	Lv int64 `json:"lv"`
-	// 升级所需时间（秒） map[Lv]Seconds
-	LvSeconds map[int64]int64 `json:"lv_seconds"`
-	ChildID   []int64         `json:"child_id"`
-}
-
 type User struct {
 	Id    int       `json:"id"`
 	Name  string    `json:"name"`
@@ -29,7 +21,3 @@ func (u *User) MarshalJSON() ([]byte, error) {
 		Xtime: u.Xtime.Unix(),
 	})
 }
-
-type pencil struct{}
-
-func (p pencil) Write([]byte) (int, error) { return 0, nil }
