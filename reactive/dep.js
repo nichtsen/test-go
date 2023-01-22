fc5d45c042eedf61186e97f6475fcad546e1a0ed
nichtsen <hobblong@163.com>
2023-01-22 13:22:04 +0800

    watch object feature

--- reactive/dep.js
+++ reactive/dep.js
@@ -4,9 +4,29 @@
 
 class Dependency {
 	constructor(state) {
-		this.state = state;
 		this.update = null;
 		this.subscribers = new Set();
+		this.state = state;
+
+		let notify = () => {
+			this.subscribers.forEach((update) => {
+				update();
+			});
+		};
+		Object.keys(state).forEach((key) => {
+			let value = state[key];
+			Object.defineProperty(state, key, {
+				get() {
+					return value;
+				},
+				set(v) {
+					value = v;
+					notify();
+				},
+				enumerable: true,
+				configurable: true,
+			});
+		});
 	}
 	dependent() {
 		if (this.update) {
@@ -33,16 +53,49 @@ class Dependency {
 
 let pow2 = 0;
 let pow3 = 0;
-let dep = new Dependency(3);
-dep.onStateChange(() => {
-	pow2 = dep.state * dep.state;
-});
-dep.onStateChange(() => {
-	pow3 = dep.state * dep.state * dep.state;
-});
-console.log(`dep ${dep.state}: pow2 ${pow2} pow3 ${pow3}`);
+let effect = (dep, extract) => {
+	dep.onStateChange(() => {
+		let count = extract(dep);
+		pow2 = count * count;
+	});
+	dep.onStateChange(() => {
+		let count = extract(dep);
+		pow3 = count * count * count;
+	});
+};
 
-dep.set((state) => {
-	return state + 1;
-});
-console.log(`dep ${dep.state}: pow2 ${pow2} pow3 ${pow3}`);
+let depOfObject = () => {
+	let state = {
+		count: 3,
+	};
+	let dep = new Dependency(state);
+	effect(dep, (dep) => {
+		return dep.state.count;
+	});
+	console.log(`dep ${dep.state.count}: pow2 ${pow2} pow3 ${pow3}`);
+
+	state.count++;
+	console.log(`dep ${dep.state.count}: pow2 ${pow2} pow3 ${pow3}`);
+};
+let depOfNumber = () => {
+	pow2 = 0;
+	pow3 = 0;
+	let dep = new Dependency(3);
+	effect(dep, (dep) => {
+		return dep.state;
+	});
+	console.log(`dep ${dep.state}: pow2 ${pow2} pow3 ${pow3}`);
+
+	dep.set((state) => {
+		return state + 1;
+	});
+
+	console.log(`dep ${dep.state}: pow2 ${pow2} pow3 ${pow3}`);
+};
+depOfObject();
+depOfNumber();
+// Output:
+// dep 3: pow2 9 pow3 27
+// dep 4: pow2 16 pow3 64
+// dep 3: pow2 9 pow3 27
+// dep 4: pow2 16 pow3 64
